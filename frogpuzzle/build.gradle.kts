c2ba7c6d009df60a3e098c63e161cd485472d03b
ojergan <ojergan@shearwatergeo.com>
2020-08-27 15:43:57 +0200

    Unit test

--- frogpuzzle/build.gradle.kts
+++ frogpuzzle/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     // Apply the java-library plugin to add support for Java Library
     scala
+    id("com.github.maiflai.scalatest") version "0.26"
 }
 
 repositories {
@@ -13,4 +14,5 @@ dependencies {
     compile("org.scala-lang:scala-library:2.13.3")
 //    testCompile("org.scalatest:scalatest_2.11:2.2.2")
     testCompile("org.scalatest:scalatest_2.13:3.2.0")
+    testRuntime("com.vladsch.flexmark:flexmark-all:0.35.10")
 }
--- frogpuzzle/src/test/scala/FirstTest.scala
+++ frogpuzzle/src/test/scala/FirstTest.scala
@@ -1,6 +1,7 @@
 import org.scalatest.flatspec.AnyFlatSpec
 
 class FirstTest extends AnyFlatSpec {
-    println("pelle");
+    println("pelle er best");
+    assert(true);
 //    assert(Pelle.m.equals("nei"))
 }
