e430e801322a4eb8e72cb259359ddecce7842aca
ojergan <ojergan@shearwatergeo.com>
2020-08-27 15:31:38 +0200

    Scala

---
+++ frogpuzzle/src/main/scala/First.scala
@@ -0,0 +1,24 @@
+
+/**
+ * What does this class do?
+ *
+ * @author <a href="mailto:ojergan@shearwatergeo.com">Oyvind Jergan</a>
+ * @version
+ */
+
+object Hello extends App {
+  println("Hello pelle")
+}
+
+object Pelle {
+
+  def m : String = {
+    "Hei"
+  }
+}
+
+object CubeCalculator extends App {
+  def cube(x: Int) = {
+    x * x * x
+  }
+}
---
+++ frogpuzzle/src/test/scala/FirstTest.scala
@@ -0,0 +1,6 @@
+import org.scalatest.flatspec.AnyFlatSpec
+
+class FirstTest extends AnyFlatSpec {
+    println("pelle");
+//    assert(Pelle.m.equals("nei"))
+}
