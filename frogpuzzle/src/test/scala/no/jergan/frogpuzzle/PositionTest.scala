f3566a9384440fc059ba7f3423568ce351e9fc80
ojergan <ojergan@shearwatergeo.com>
2020-08-28 12:03:43 +0200

    Frog.

--- frogpuzzle/src/test/scala/no/jergan/frogpuzzle/PositionTest.scala
+++ frogpuzzle/src/test/scala/no/jergan/frogpuzzle/PositionTest.scala
@@ -13,9 +13,17 @@ class PositionTest extends AnyFlatSpec {
   behavior of "move"
 
   it should "be able to move without action" in {
-    val newPosition = new Position(2, 2).move(None, FACE_UP);
-    println(newPosition);
-    assert(new Position(2, 2).move(None, FACE_UP).equals(new Position(2, 1)))
+    assert(Position(2, 2).move(None, FACE_UP).equals(Position(2, 1)))
+    assert(Position(2, 2).move(None, FACE_RIGHT).equals(Position(3, 2)))
+    assert(Position(2, 2).move(None, FACE_DOWN).equals(Position(2, 3)))
+    assert(Position(2, 2).move(None, FACE_LEFT).equals(Position(1, 2)))
+  }
+
+  it should "be able to move with action" in {
+    assert(Position(2, 2).move(Some(MOVE_UP), FACE_LEFT).equals(Position(2, 1)))
+    assert(Position(2, 2).move(Some(MOVE_RIGHT), FACE_UP).equals(Position(3, 2)))
+    assert(Position(2, 2).move(Some(MOVE_DOWN), FACE_RIGHT).equals(Position(2, 3)))
+    assert(Position(2, 2).move(Some(MOVE_LEFT), FACE_DOWN).equals(Position(1, 2)))
   }
 
 }
