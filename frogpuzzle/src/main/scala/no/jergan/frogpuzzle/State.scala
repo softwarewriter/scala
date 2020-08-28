0ff0c92aeb0f82498f0380d4b989cc7403610b84
ojergan <ojergan@shearwatergeo.com>
2020-08-28 20:53:53 +0200

    Refactoring.

--- frogpuzzle/src/main/scala/no/jergan/frogpuzzle/State.scala
+++ frogpuzzle/src/main/scala/no/jergan/frogpuzzle/State.scala
@@ -12,11 +12,12 @@ case class State(val position: Position, val orientation: Orientation) {
    }
 
    def move(action: Option[Action]): State = {
+      val next = position.move(action, orientation)
       (action, orientation) match {
-         case (None, FACE_UP) | (Some(MOVE_UP), _) => State(position.move(action, orientation), FACE_UP);
-         case (None, FACE_RIGHT) | (Some(MOVE_RIGHT), _) => State(position.move(action, orientation), FACE_RIGHT);
-         case (None, FACE_DOWN) | (Some(MOVE_DOWN), _) => State(position.move(action, orientation), FACE_DOWN);
-         case (None, FACE_LEFT) | (Some(MOVE_LEFT), _) => State(position.move(action, orientation), FACE_LEFT);
+         case (None, FACE_UP) | (Some(MOVE_UP), _) => State(next, FACE_UP);
+         case (None, FACE_RIGHT) | (Some(MOVE_RIGHT), _) => State(next, FACE_RIGHT);
+         case (None, FACE_DOWN) | (Some(MOVE_DOWN), _) => State(next, FACE_DOWN);
+         case (None, FACE_LEFT) | (Some(MOVE_LEFT), _) => State(next, FACE_LEFT);
       }
    }
 
