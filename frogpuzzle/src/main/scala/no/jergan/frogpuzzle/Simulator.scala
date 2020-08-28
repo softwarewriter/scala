fad07230745e1e20b39aac79e25e7536826761ac
ojergan <ojergan@shearwatergeo.com>
2020-08-28 18:19:49 +0200

    Tail recursion in simulator.

--- frogpuzzle/src/main/scala/no/jergan/frogpuzzle/Simulator.scala
+++ frogpuzzle/src/main/scala/no/jergan/frogpuzzle/Simulator.scala
@@ -1,5 +1,6 @@
 package no.jergan.frogpuzzle
 
+import scala.collection.convert.ImplicitConversionsToJava.`set AsJavaSet`
 import scala.collection.mutable
 
 /**
@@ -10,27 +11,22 @@ import scala.collection.mutable
 class Simulator {
 
    def correctSolution(solution: Solution): Boolean = {
-      val unvisited = new mutable.HashSet[Position].addAll(solution.board.requiredToVisit())
-      correctSolution(solution, Set.empty, unvisited, solution.board.initialState());
+      correctSolution(solution, solution.board.requiredToVisit(), Set.empty, solution.board.initialState());
    }
 
-   private[this] def correctSolution(solution: Solution, states: Set[State], unvisited: mutable.HashSet[Position], state: State): Boolean = {
+   @scala.annotation.tailrec
+   private[this] def correctSolution(solution: Solution, requiredToVisit: Set[Position], states: Set[State], state: State): Boolean = {
       if (states.contains(state)) {
          return false; // loop
       }
       val position = state.position;
-      if (position == solution.board.end() && unvisited.isEmpty) {
+      if (solution.board.squareAt(position) == EMPTY) {
+         return false; // outside
+      }
+      if (position == solution.board.end() && states.map(state => state.position).containsAll(requiredToVisit)) {
          return true;
       }
-      if (solution.board.squareAt(position) == EMPTY) {
-         return false;
-      }
-      val removed = unvisited.remove(position);
-      val result = correctSolution(solution, states + state, unvisited, state.move(solution.actionAt(position)))
-      if (removed) {
-         unvisited.add(position);
-      }
-      result;
+      correctSolution(solution, requiredToVisit, states + state, state.move(solution.actionAt(position)))
    }
 
 }
