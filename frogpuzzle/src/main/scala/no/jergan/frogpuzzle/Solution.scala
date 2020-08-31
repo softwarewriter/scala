0bf87f2d4c6a9c1b4e20a9a3d880d6f58ffedaaa
ojergan <ojergan@shearwatergeo.com>
2020-08-31 11:33:16 +0200

    Replace Exception by Either.

--- frogpuzzle/src/main/scala/no/jergan/frogpuzzle/Solution.scala
+++ frogpuzzle/src/main/scala/no/jergan/frogpuzzle/Solution.scala
@@ -7,20 +7,20 @@ package no.jergan.frogpuzzle
  */
 class Solution private(val board: Board, positionToAction: Map[Position, Action]) {
 
-   def addAction(x: Int, y: Int, action: Action): Solution = {
+   def addAction(x: Int, y: Int, action: Action): Either[String, Solution] = {
       addAction(Position(x, y), action)
    }
 
-   def addAction(position: Position, action: Action): Solution = {
+   def addAction(position: Position, action: Action): Either[String, Solution] = {
       val existing = positionToAction.get(position)
       if (existing.isDefined) {
-         throw new Exception(s"Can't add action to position $position as position already has action $existing")
+         Left(s"Can't add action to position $position as position already has action $existing")
       }
       val square = board.squareAt(position)
       if (square != REGULAR) {
-         throw new Exception(s"Actions can only be put on regular squares, was $square")
+         Left(s"Actions can only be put on regular squares, was $square")
       }
-      new Solution(board, positionToAction + (position -> action))
+      Right(new Solution(board, positionToAction + (position -> action)))
    }
 
    def actionAt(position: Position): Option[Action] = {
@@ -32,4 +32,15 @@ object Solution {
    def empty(board: Board): Solution = {
       new Solution(board, Map.empty)
    }
+
+   def build(board: Board, values: Tuple3[Int, Int, Action]*) : Either[String, Solution] = {
+      val zero: Either[String, Solution] = Right(empty(board))
+      values.foldRight(zero) ((element: Tuple3[Int, Int, Action], solution) => {
+         solution match {
+            case Left(value) => Left(value)
+            case Right(value) => value.addAction(element._1, element._2, element._3)
+         }
+      })
+   }
+
 }
--- frogpuzzle/src/test/scala/no/jergan/frogpuzzle/SimulatorTest.scala
+++ frogpuzzle/src/test/scala/no/jergan/frogpuzzle/SimulatorTest.scala
@@ -13,38 +13,39 @@ class SimulatorTest extends AnyFlatSpec {
 
    it should "be able to identify correct solution" in {
       val board = FrogPuzzleTest.testBoard()
-      val solution = Solution.empty(board)
-         .addAction(1, 2, MOVE_RIGHT)
-         .addAction(2, 2, MOVE_DOWN)
-         .addAction(2, 3, MOVE_RIGHT)
-         .addAction(3, 3, MOVE_UP)
+
+      val solution = Solution.build(board,
+         (1, 2, MOVE_RIGHT),
+         (2, 2, MOVE_DOWN),
+         (2, 3, MOVE_RIGHT),
+         (3, 3, MOVE_UP)).getOrElse(fail())
       assert(new Simulator().correctSolution(solution))
    }
 
    it should "be able to identify correct solution on board with overlap" in {
       val board = FrogPuzzleTest.testBoardWithOverlap()
-      val solution = Solution.empty(board)
-         .addAction(2, 1, MOVE_RIGHT)
-         .addAction(3, 1, MOVE_DOWN)
-         .addAction(3, 2, MOVE_LEFT)
-         .addAction(1, 2, MOVE_UP)
+      val solution = Solution.build(board,
+         (2, 1, MOVE_RIGHT),
+         (3, 1, MOVE_DOWN),
+         (3, 2, MOVE_LEFT),
+         (1, 2, MOVE_UP)).getOrElse(fail())
       assert(new Simulator().correctSolution(solution))
    }
 
    it should "be able to identify non correct solution" in {
       val board = FrogPuzzleTest.testBoard()
-      val solution = Solution.empty(board)
-         .addAction(1, 2, MOVE_RIGHT)
-         .addAction(2, 2, MOVE_DOWN)
-         .addAction(2, 3, MOVE_RIGHT)
+      val solution = Solution.build(board,
+         (1, 2, MOVE_RIGHT),
+         (2, 2, MOVE_DOWN),
+         (2, 3, MOVE_RIGHT)).getOrElse(fail())
       assert(!new Simulator().correctSolution(solution))
    }
 
    it should "be able to identify non correct solution caused by loops" in {
       val board = FrogPuzzleTest.testBoard()
-      val solution = Solution.empty(board)
-         .addAction(1, 3, MOVE_UP)
-         .addAction(1, 2, MOVE_DOWN)
+      val solution = Solution.build(board,
+         (1, 3, MOVE_UP),
+         (1, 2, MOVE_DOWN)).getOrElse(fail())
       assert(!new Simulator().correctSolution(solution))
    }
 
