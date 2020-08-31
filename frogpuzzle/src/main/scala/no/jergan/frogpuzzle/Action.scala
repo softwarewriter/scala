da535c2c23e78ea723f7c7cd047b70e616eebf02
ojergan <ojergan@shearwatergeo.com>
2020-08-31 15:32:01 +0200

    Refactoring. (Should still have type Move as common supertype for Move actions to get cleaner matching.)

--- frogpuzzle/src/main/scala/no/jergan/frogpuzzle/Action.scala
+++ frogpuzzle/src/main/scala/no/jergan/frogpuzzle/Action.scala
@@ -6,28 +6,33 @@ package no.jergan.frogpuzzle
  * @author <a href="mailto:oyvind@jergan.no">Oyvind Jergan</a>
  */
 sealed trait Action {
-
    def character: Char
+   def orientation: Option[Orientation]
 }
 
 case object MOVE_UP extends Action {
    val character = 'u'
+   val orientation = Some(FACE_UP)
 }
 
 case object MOVE_RIGHT extends Action {
    val character = 'r'
+   val orientation = Some(FACE_RIGHT)
 }
 
 case object MOVE_DOWN extends Action {
    val character = 'd'
+   val orientation = Some(FACE_DOWN)
 }
 
 case object MOVE_LEFT extends Action {
    val character = 'l'
+   val orientation = Some(FACE_LEFT)
 }
 
 case object JUMP extends Action {
    val character = 'j'
+   val orientation = None
 }
 
 object Action {
--- frogpuzzle/src/main/scala/no/jergan/frogpuzzle/Position.scala
+++ frogpuzzle/src/main/scala/no/jergan/frogpuzzle/Position.scala
@@ -13,11 +13,9 @@ case class Position(x: Int, y: Int) {
 
    def move(action: Option[Action], orientation: Orientation): Position = {
       (action, orientation) match {
-         case (None, FACE_UP) | (Some(MOVE_UP), _) => FACE_UP.next(this)
-         case (None, FACE_RIGHT) | (Some(MOVE_RIGHT), _) => FACE_RIGHT.next(this)
-         case (None, FACE_DOWN) | (Some(MOVE_DOWN), _) => FACE_DOWN.next(this)
-         case (None, FACE_LEFT) | (Some(MOVE_LEFT), _) => FACE_LEFT.next(this)
+         case (None, _) => orientation.next(this)
          case (Some(JUMP), _) => orientation.next(orientation.next(this))
+         case (Some(a), _) => a.orientation.get.next(this)
       }
    }
 
