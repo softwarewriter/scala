33c5bd9c3056f2edd4d0b0cde343c62a3586dfc2
ojergan <ojergan@shearwatergeo.com>
2020-09-16 10:22:20 +0200

    SBT project

---
+++ list/src/main/scala/no/jergan/list/Halla.scala
@@ -0,0 +1,13 @@
+package no.jergan.list
+
+/**
+ * What does this class do?
+ *
+ * @author <a href="mailto:oyvind@jergan.no">Oyvind Jergan</a>
+ */
+object Halla {
+
+   def hei(): Int = {
+      42
+   }
+}
