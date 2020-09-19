9405cfaaa8dc6706fd21669e842cefb6fc5c83d7
ojergan <ojergan@shearwatergeo.com>
2020-09-19 21:53:41 +0200

    List

--- frogpuzzle/src/main/scala/no/jergan/frogpuzzle/package.scala
+++ frogpuzzle/src/main/scala/no/jergan/frogpuzzle/package.scala
@@ -23,16 +23,19 @@ package object frogpuzzle {
    implicit class StringableSyntax[A](val a: A) {
       def asStringInSyntax(implicit stringable: Stringable[A]): String = stringable.asString(a)
    }
+   // @kaare:
+   //  Dette er signaturen vi brukte på fredag:
+   //    implicit class StringableSyntax[A: Stringable](val a: A) {
 
    implicit var positionStringable = new Stringable[Position] {
       override def asString(position: Position): String = s"(${position.x}, ${position.y})"
    }
 
-   implicit var orientationStringable = new Stringable[Orientation] {
-      override def asString(orientation: Orientation): String = orientation.toString
-   }
-
    // Variant i kortform (single abstract method)
-   implicit var stateStringable: Stringable[State] = (state: State) => s"(${positionStringable.asString(state.position)} - ${orientationStringable.asString(state.orientation)})"
+   implicit var orientationStringable: Stringable[Orientation] = (orientation: Orientation) => orientation.toString
+
+   implicit var stateStringable: Stringable[State] = new Stringable[State] {
+      override def asString(state: State): String = s"(${positionStringable.asString(state.position)} - ${orientationStringable.asString(state.orientation)})"
+   }
 
 }
