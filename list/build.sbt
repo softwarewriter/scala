a1209d5bcc4ad8f107edc151194de9533c89345f
ojergan <ojergan@shearwatergeo.com>
2020-09-22 10:00:34 +0200

    Hei

--- list/build.sbt
+++ list/build.sbt
@@ -5,5 +5,3 @@ scalaVersion := "2.13.3"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.0" % Test
 
 libraryDependencies += "org.typelevel" %% "cats-effect" % "2.1.4" withSources() withJavadoc()
-
-libraryDependencies += "com.pepegar" %% "hammock-core" % "0.10.0"
--- list/src/main/scala/no/jergan/list/CatsHammock.scala
+++ list/src/main/scala/no/jergan/list/CatsHammock.scala
@@ -1,10 +1,5 @@
 package no.jergan.list
 
-import cats.effect.IO
-import hammock._
-import hammock.marshalling._
-import hammock.apache.ApacheInterpreter
-import hammock.circe.implicits._
 
 /**
  * What does this class do?
@@ -14,17 +9,6 @@ import hammock.circe.implicits._
 object CatsHammock {
 
    def main(args: Array[String]): Unit = {
-
-
-
-      object HttpClient {
-         // Using the Apache HTTP commons interpreter
-         implicit val interpreter = ApacheInterpreter.instance[IO]
-
-         val response = Hammock
-            .request(Method.GET, uri"https://api.fidesmo.com/apps", Map()) // In the `request` method, you describe your HTTP request
-            .as[List[String]]
-            .exec[IO]
-      }
+      println("pelle")
    }
 }
