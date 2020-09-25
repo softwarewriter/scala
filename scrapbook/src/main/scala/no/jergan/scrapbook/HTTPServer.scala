510f5add44f8a35abb0fc3b49f998cf93e16d80c
ojergan <ojergan@shearwatergeo.com>
2020-09-25 18:00:54 +0200

    Moved implicit to companion object

--- scrapbook/src/main/scala/no/jergan/scrapbook/HTTPServer.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/HTTPServer.scala
@@ -1,7 +1,7 @@
 package no.jergan.scrapbook
 
 import cats.effect.{ExitCode, IO, IOApp}
-import io.circe.{Encoder, Json}
+import io.circe.{Encoder, Json, JsonObject}
 import org.http4s.{HttpApp, HttpRoutes, Method, Request, Response, Status, Uri}
 import org.http4s.dsl.io._
 import org.http4s.implicits._
@@ -9,61 +9,80 @@ import org.http4s.server.blaze.BlazeServerBuilder
 import io.circe.syntax._
 
 /**
-  * HTTP server for person.
-  *
-  * @author <a href="mailto:oyvind@jergan.no">Oyvind Jergan</a>
-  */
+ * HTTP server for person.
+ *
+ * @author <a href="mailto:oyvind@jergan.no">Oyvind Jergan</a>
+ */
 object HTTPServer extends IOApp {
 
-  case class Person(firstName: String, lastName: String, age: Int, uncle: Option[Person] = None)
+   case class Person(firstName: String, lastName: String, age: Int, uncle: Option[Person] = None)
 
-  object Person {
-     implicit def personToOption(person: Person) = Some(person)
-  }
+   def personService(id: String): Option[Person] = {
+      val onkelSkrue  = new Person("Onkel", "Skrue", 78)
+      val onkelDonald = new Person("Donald", "Duck", 35, Some(onkelSkrue))
+      val database = Map(
+         "0" -> onkelSkrue,
+         "1" -> onkelDonald,
+         "2" -> new Person("Ole", "Duck", 9, Some(onkelDonald)),
+         "3" -> new Person("Dole", "Duck", 8, Some(onkelDonald)),
+         "4" -> new Person("Doffen", "Duck", 6, Some(onkelDonald)),
+         "5" -> new Person("Nabo", "Jensen", 42),
+      )
+      database.get(id)
+   }
 
-  def personService(id: String): Option[Person] = {
-    val onkelSkrue  = new Person("Onkel", "Skrue", 78)
-    val onkelDonald = new Person("Donald", "Duck", 35, onkelSkrue)
-    val database = Map(
-      "0" -> onkelSkrue,
-      "1" -> onkelDonald,
-      "2" -> new Person("Ole", "Duck", 9, onkelDonald),
-      "3" -> new Person("Dole", "Duck", 8, onkelDonald),
-      "4" -> new Person("Doffen", "Duck", 6, onkelDonald),
-      "5" -> new Person("Nabo", "Jensen", 42),
-    )
-    database.get(id)
-  }
+   implicit val personEncoder: Encoder[Person] = (person: Person) => {
 
-  // @kaare: hvordan (elegant) lage en variabel liste av elementer (slik at jeg slipper "no uncle").
-  implicit val personEncoder: Encoder[Person] = (person: Person) =>
-    Json.obj(
-      ("firstName", Json.fromString(person.firstName)),
-      ("lastName", Json.fromString(person.lastName)),
-      ("age", Json.fromInt(person.age)),
-      person.uncle match {
-        case Some(uncle) => ("uncle", uncle.asJson)
-        case None        => ("no uncle", Json.fromString(""))
+     val l1: List[Int] = List(1, 2, 3)
+     val l2: List[Int] = List(4, 5, 6)
+
+     val l3: List[Int] = l1.appendedAll(l2)
+
+      val elements: List[(String, Json)] = List("firstName" -> Json.fromString(person.firstName),
+         "lastName" -> Json.fromString(person.lastName),
+         "age"      -> Json.fromInt(person.age))
+
+      val elements1 : List[(String, Json)] = person.uncle.map(uncle => List("uncle" -> uncle.asJson)).getOrElse(Nil)
+
+      //         person.uncle.map(uncle => List("uncle" -> uncle.asJson)).getOrElse(Nil)
+//      val elements1 : List[(String, Json)] = person.uncle.map(uncle => List("uncle" -> uncle.asJson)).getOrElse(Nil)
+      val elements2: List[(String, Json)] = elements.appendedAll(elements1)
+
+      Json.obj(elements2: _*)
+
+      /*
+        Json.obj(
+           "firstName":= person.firstName,
+           "lastName":= person.   lastName,
+           "age":= person.age,
+           "uncle":= person.uncle
+        )
+
+     */
+   }
+
+   implicit def entityEncoder[A: Encoder] = org.http4s.circe.jsonEncoderOf[IO, A]
+
+   // /vessels/performance/speed
+   // /vessels/ {imo} / speed
+
+   val personHttpService: HttpRoutes[IO] = HttpRoutes.of[IO] {
+      case GET -> Root / "person" / id => {
+         personService(id) match {
+            case (None)         => NotFound("No person with id " + id)
+            case (Some(person)) => Ok(person)
+         }
       }
-  )
+   }
 
-  val personHttpService: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root / "person" / id => {
-      personService(id) match {
-        case (None)         => NotFound("No person with id " + id)
-        case (Some(person)) => Ok(person.asJson.toString())
-      }
-    }
-  }
-
-  override def run(args: List[String]): IO[ExitCode] = {
-    BlazeServerBuilder[IO](scala.concurrent.ExecutionContext.global)
-      .bindHttp(6510)
-      .withHttpApp(personHttpService.orNotFound)
-      .serve
-      .compile
-      .drain
-      .map(_ => ExitCode.Success)
-  }
+   override def run(args: List[String]): IO[ExitCode] = {
+      BlazeServerBuilder[IO](scala.concurrent.ExecutionContext.global)
+         .bindHttp(8080)
+         .withHttpApp(personHttpService.orNotFound)
+         .serve
+         .compile
+         .drain
+         .map(_ => ExitCode.Success)
+   }
 
 }
