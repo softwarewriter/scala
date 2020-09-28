3def6f16a87994135766f7aa199a92605c4eecb2
ojergan <ojergan@shearwatergeo.com>
2020-09-28 13:35:10 +0200

    Scrap.

--- scrapbook/src/main/scala/no/jergan/scrapbook/Scrap.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/Scrap.scala
@@ -77,4 +77,40 @@ object Scrap extends App {
     case (_) => println("røkla")
   }
 
+  abstract class AbsIterator {
+    type T
+    def hasNext: Boolean
+    def next(): T
+  }
+
+  class StringIterator(s: String) extends AbsIterator {
+    type T = Char
+    private var i = 0
+    def hasNext = i < s.length
+    def next() = {
+      val ch = s charAt i
+      i += 1
+      ch
+    }
+  }
+
+  def urlBuilder(ssl: Boolean, domainName: String): (String, String) => String = {
+    val schema = if (ssl) "https://" else "http://"
+    (endpoint: String, query: String) => s"$schema$domainName/$endpoint?$query"
+  }
+
+  val domainName = "www.example.com"
+  def getURL = urlBuilder(ssl=true, domainName)
+  val endpoint = "users"
+  val query = "id=1"
+  val url = getURL(endpoint, query) // "https://www.example.com/users?id=1": String
+
+  def factorial(x: Int): Int = {
+    def fact(x: Int, accumulator: Int): Int = {
+      if (x <= 1) accumulator
+      else fact(x - 1, x * accumulator)
+    }
+    fact(x, 1)
+  }
+
 }
