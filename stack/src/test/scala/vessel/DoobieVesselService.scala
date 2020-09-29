9cca19584e88cb04a42bad8910dd9ee4e5fe2236
ojergan <ojergan@shearwatergeo.com>
2020-09-29 14:15:51 +0200

    Doobie

---
+++ stack/src/test/scala/vessel/DoobieVesselService.scala
@@ -0,0 +1,18 @@
+package vessel
+
+/**
+ * Implementation of [[VesselService]] that targets doobie.
+ *
+ * @author <a href="mailto:oyvind@jergan.no">Oyvind Jergan</a>
+ */
+class DoobieVesselService extends VesselService {
+
+   override def get(imo: String): Option[Vessel] = ???
+
+   override def put(vessel: Vessel): Unit = ???
+
+   override def delete(imo: String): Option[Vessel] = ???
+
+   override def search(query: String): List[Vessel] = ???
+
+}
