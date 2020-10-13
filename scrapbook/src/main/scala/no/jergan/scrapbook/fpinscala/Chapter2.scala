55e0e718cdb42feb01a77eb8272ce3b7abe159e3
oyvind <oyvind@jergan.no>
2020-10-13 09:52:01 +0200

    Ex3

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter2.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter2.scala
@@ -15,17 +15,32 @@ object Chapter2 {
     }
 
     def test(): Unit = {
-      val f: (Int, Int) => Boolean = (a: Int, b: Int) => a > b
+      val f: (Int, Int) => Boolean = (a, b) => a > b
       println(Ex2.isSorted(List().toArray, f))
       println(Ex2.isSorted(List(1).toArray, f))
       println(Ex2.isSorted(List(1, 2, 3).toArray, f))
-      println(Ex2.isSorted(List(1, 3, 2).toArray, f))
+      println(!Ex2.isSorted(List(1, 3, 2).toArray, f))
     }
+  }
+
+  object Ex3 {
+
+    def partial1[A,B,C](a: A, f: (A,B) => C): B => C = {
+      (b: B) => f(a, b)
+    }
+
+    def test(): Unit = {
+      val f: (Int => String) = partial1[String, Int, String]("ole", (a: String, b: Int) => String.valueOf(a.length + b))
+
+      println(f(3))
+    }
+
+
   }
 
   def main(args: Array[String]): Unit = {
 
-    Ex2.test()
+    Ex3.test()
   }
 
 }
