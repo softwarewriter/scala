83cbb3b12c7fffa0aff6210d566ac98b62ad782f
oyvind <oyvind@jergan.no>
2020-10-24 18:17:46 +0200

    Ch5

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter5.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter5.scala
@@ -10,10 +10,14 @@ object Chapter5 {
 
   object Stream {
     def empty[A]: Stream[A] =
-      new Stream[A] { def uncons = None }
+      new Stream[A] {
+        def uncons = None
+      }
 
     def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
-      new Stream[A] { lazy val uncons = Some((hd, tl)) }
+      new Stream[A] {
+        lazy val uncons = Some((hd, tl))
+      }
 
     def apply[A](as: A*): Stream[A] =
       if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
