64b262698dfe732a221311a830d9553b93cb8eb7
oyvind <oyvind@jergan.no>
2020-10-29 07:26:01 +0100

    Ch5

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter5.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter5.scala
@@ -7,6 +7,8 @@ object Chapter5 {
 
     def toList: List[A]
 
+    def take(n: Int): Stream[A]
+
     def isEmpty: Boolean = uncons.isEmpty
 
   }
@@ -17,16 +19,27 @@ object Chapter5 {
         def uncons = None
 
         override def toList: List[A] = List.empty
+
+        override def take(n: Int): Stream[A] = empty
+
       }
 
     def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
       new Stream[A] {
         lazy val uncons = Some((hd, tl))
 
-        override def toList: List[A] = {
-          val s = uncons.get
-          List(s._1) ++ s._2.toList
-        }
+        override def toList: List[A] = uncons match {
+            case Some((hd, tl)) => List(hd) ++ tl.toList
+          }
+
+        /**
+         * Assume returned stream is original stream minus n elements (n elements arew thrown away).
+         */
+        override def take(n: Int): Stream[A] =
+          if (n == 0 || isEmpty) this else
+            uncons match {
+              case Some((hd, tl)) => tl.take(n - 1)
+            }
       }
 
     def apply[A](as: A*): Stream[A] =
@@ -36,27 +49,6 @@ object Chapter5 {
   object Ex1 {
 
     def test(): Unit = {
-
-      val l = List(1, 2, 3)
-
-      val block: Int = {
-        println("block")
-        1
-      }
-
-      def m(): Int = {
-        println("m")
-        1
-      }
-
-      def requiredInt(i: => Int) = {
-
-        lazy val j  = i;
-//        println(j * j)
-      }
-
-      requiredInt(m)
-
     }
   }
 
