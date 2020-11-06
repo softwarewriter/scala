d90aad599bb35ede73e3885ddac4d1dbe4a2d774
oyvind <oyvind@jergan.no>
2020-11-06 10:11:54 +0100

    ch6

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter6.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter6.scala
@@ -12,18 +12,28 @@ object Chapter6 {
     def nextInt: (Int, RNG)
   }
 
+  case class SimpleRNG(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
-
     val (a, nextRng) = rng.nextInt
-
-
-
-
+    a match {
+      case (Int.MinValue) => nonNegativeInt(nextRng)
+      case _ => if (a >= 0) (a, nextRng) else (a.abs, nextRng)
+    }
   }
 
   object Ex1 {
     def test(): Unit = {
-      println("pelle2")
+
+      val rng = SimpleRNG(1)
+      println(nonNegativeInt(rng))
     }
   }
 
