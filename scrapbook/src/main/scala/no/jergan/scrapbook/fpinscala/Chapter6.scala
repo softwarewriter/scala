00720ab6248c982743dad134a55c4baab93f27ce
oyvind <oyvind@jergan.no>
2020-11-06 10:00:44 +0100

    ch6

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter6.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter6.scala
@@ -1,8 +1,34 @@
 package no.jergan.scrapbook.fpinscala
 
+import no.jergan.scrapbook.fpinscala.Chapter5.Stream
+
 /**
  * First chapter done according to new version of book.
  */
 object Chapter6 {
+
+
+  trait RNG {
+    def nextInt: (Int, RNG)
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+
+    val (a, nextRng) = rng.nextInt
+
+
+
+
+  }
+
+  object Ex1 {
+    def test(): Unit = {
+      println("pelle2")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    Ex1.test()
+  }
 
 }
