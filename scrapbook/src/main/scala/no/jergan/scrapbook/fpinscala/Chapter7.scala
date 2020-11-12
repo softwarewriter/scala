4bc74eeecbb526b0881726e31f7559c13a61a6be
oyvind <oyvind@jergan.no>
2020-11-12 07:56:09 +0100

    ch7

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter7.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter7.scala
@@ -1,40 +1,67 @@
 package no.jergan.scrapbook.fpinscala
 
 import scala.annotation.tailrec
+import scala.concurrent.duration.TimeUnit
 
 /**
  * Second chapter done according to new version of book.
  */
 object Chapter7 {
 
-  trait Par[A]
+  case class Par[A](f: () => A)
 
   object Par {
 
-    def unit[A](a: => A): Par[A] = ???
+    def unit[A](a: A): Par[A] = ???
 
-    def get[A](a: Par[A]): A = ???
+    def lazyUnit[A](a: => A): Par[A] = {
+      fork(unit(a))
+    }
+
+    def run[A](pa: Par[A]): A = ???
 
     def map2[A](pl: Par[A], pr: Par[A])(f: (A, A) => A): Par[A] = ???
 
-    //  Par.map2(sum(l), sum(r))(_ + _)
-
+    def fork[A](pa: => Par[A]): Par[A] = ???
 
   }
 
-  def sum(ints: IndexedSeq[Int]): Par[Int] =
+  def sum(ints: List[Int]): Par[Int] = {
     if (ints.size <= 1)
-    Par.unit(ints.headOption getOrElse 0) else {
-    val (l, r) = ints.splitAt(ints.length / 2)
-    Par.map2(sum(l), sum(r))(_ + _)
+      Par.unit(ints.headOption.getOrElse(0))
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2)
+      Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_ + _)
+    }
+  }
+
+  class ExecutorService {
+    def submit[A](a: Callable[A]): Future[A] = ???
+  }
+
+  trait Callable[A] {
+    def call: A }
+
+  trait Future[A] {
+    def get: A
+    def get(timeout: Long, unit: TimeUnit): A
+    def cancel(evenIfRunning: Boolean): Boolean
+    def isDone: Boolean
+    def isCancelled: Boolean
   }
 
   object Ex1 {
 
     def test(): Unit = {
 
+      type Par[A] = ExecutorService => Future[A]
 
-    }
+      def run[A](s: ExecutorService)(a: Par[A]): Future[A] = {
+        a(s)
+      }
+
+
+
   }
 
   def main(args: Array[String]): Unit = {
