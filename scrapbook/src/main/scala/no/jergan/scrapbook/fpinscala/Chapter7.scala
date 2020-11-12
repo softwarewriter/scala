40e1e439ca81a71305dd3b938a9065a3cf081bb0
oyvind <oyvind@jergan.no>
2020-11-12 08:45:03 +0100

    ch7

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter7.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter7.scala
@@ -8,39 +8,6 @@ import scala.concurrent.duration.TimeUnit
  */
 object Chapter7 {
 
-  type Par[A] = ExecutorService => Future[A]
-
-  object Par {
-
-    def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
-
-    private case class UnitFuture[A](get: A) extends Future[A] {
-      def isDone = true
-      def get(timeout: Long, units: TimeUnit) = get
-      def isCancelled = false
-      def cancel(evenIfRunning: Boolean): Boolean = false
-    }
-
-    def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
-      val af = a(es)
-      val bf = b(es)
-      UnitFuture(f(af.get, bf.get))
-    }
-
-    def fork[A](a: => Par[A]): Par[A] = es => es.submit(new Callable[A] {
-      def call = a(es).get
-    })
-  }
-
-  def sum(ints: List[Int]): Par[Int] = {
-    if (ints.size <= 1)
-      Par.unit(ints.headOption.getOrElse(0))
-    else {
-      val (l, r) = ints.splitAt(ints.length / 2)
-      Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_ + _)
-    }
-  }
-
   class ExecutorService {
     def submit[A](a: Callable[A]): Future[A] = ???
   }
@@ -51,14 +18,49 @@ object Chapter7 {
 
   trait Future[A] {
     def get: A
-
     def get(timeout: Long, unit: TimeUnit): A
-
     def cancel(evenIfRunning: Boolean): Boolean
-
     def isDone: Boolean
-
     def isCancelled: Boolean
+  }
+
+  type Par[A] = ExecutorService => Future[A]
+
+  object Par {
+
+    def unit[A](a: A): Par[A] = {
+      (_: ExecutorService) => UnitFuture(a)
+    }
+
+    private case class UnitFuture[A](get: A) extends Future[A] {
+      def isDone = true
+      def get(timeout: Long, units: TimeUnit) = get
+      def isCancelled = false
+      def cancel(evenIfRunning: Boolean): Boolean = false
+    }
+
+    def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = {
+      es => {
+        val af = a(es)
+        val bf = b(es)
+        UnitFuture(f(af.get, bf.get))
+      }
+    }
+
+    def fork[A](a: => Par[A]): Par[A] = {
+      es => es.submit(new Callable[A] {
+        def call = a(es).get
+      })
+    }
+  }
+
+  def sum(ints: List[Int]): Par[Int] = {
+    if (ints.size <= 1)
+      Par.unit(ints.headOption.getOrElse(0))
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2)
+      Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_ + _)
+    }
   }
 
   object Ex1 {
