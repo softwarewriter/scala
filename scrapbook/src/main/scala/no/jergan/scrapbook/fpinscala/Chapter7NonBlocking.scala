8f4df64605418cc30707d9344d0ae32dc74498f2
oyvind <oyvind@jergan.no>
2020-11-17 07:24:49 +0100

    ch7

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter7NonBlocking.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter7NonBlocking.scala
@@ -10,21 +10,14 @@ import java.util.concurrent.{Callable, CountDownLatch, ExecutorService, Executor
  */
 object Chapter7NonBlocking {
 
-  /*
-  class ExecutorService {
-    def submit[A](a: Callable[A]): Future[A] = ???
-  }
-
- */
-
   sealed trait Future[A] {
     private[fpinscala] def apply(k: A => Unit): Unit
   }
 
-  type Par[A] = ExecutorService => Future[A]
+  type Par[A] = ExecutorService => Future[Either[Exception, A]]
 
-  def run[A](es: ExecutorService)(p: Par[A]): A = {
-    val ref = new AtomicReference[A]
+  def run[A](es: ExecutorService)(p: Par[A]): Either[Exception, A] = {
+    val ref = new AtomicReference[Either[Exception, A]]
     val latch = new CountDownLatch(1)
     p(es) { a => ref.set(a); latch.countDown }
     latch.await
@@ -34,8 +27,8 @@ object Chapter7NonBlocking {
   object Par {
     def unit[A](a: A): Par[A] = {
       _ =>
-        new Future[A] {
-          def apply(cb: A => Unit): Unit = cb(a)
+        new Future[Either[Exception, A]] {
+          def apply(callback: Either[Exception, A] => Unit): Unit = callback(Right(a))
         }
     }
 
@@ -45,9 +38,17 @@ object Chapter7NonBlocking {
 
     def fork[A](a: => Par[A]): Par[A] = {
       es =>
-        new Future[A] {
-          def apply(cb: A => Unit): Unit =
-            eval(es)(a(es)(cb))
+        new Future[Either[Exception, A]] {
+          def apply(cb: Either[Exception, A] => Unit): Unit = {
+            try {
+              val v: Future[Either[Exception, A]] = a(es)
+              eval(es)(v(cb))
+              //            eval(es)(a(es)(cb))
+            }
+            catch {
+              case e: Exception => {println("fant deg"); println(e); cb(Left(e))}
+            }
+          }
         }
     }
 
@@ -80,20 +81,20 @@ object Chapter7NonBlocking {
     }
 
     def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] =
-      es => new Future[C] {
-        def apply(cb: C => Unit): Unit = {
-          var ar: Option[A] = None
-          var br: Option[B] = None
+      es => new Future[Either[Exception, C]] {
+        def apply(cb: Either[Exception, C] => Unit): Unit = {
+          var ar: Option[Either[Exception, A]] = None
+          var br: Option[Either[Exception, B]] = None
 
-          val combiner = Actor[Either[A, B]](es) {
+          val combiner = Actor[Either[Either[Exception, A], Either[Exception, B]]](es) {
             case Left(a) => br match {
               case None => ar = Some(a)
-              case Some(b) => eval(es)(cb(f(a, b)))
+              case Some(b) => eval(es)(cb(combine(a, b, f)))
             }
 
             case Right(b) => ar match {
               case None => br = Some(b)
-              case Some(a) => eval(es)(cb(f(a, b)))
+              case Some(a) => eval(es)(cb(combine(a, b, f)))
             }
           }
 
@@ -102,9 +103,45 @@ object Chapter7NonBlocking {
         }
       }
 
+    def combine[A, B, C](ea: Either[Exception, A], eb: Either[Exception, B], f: (A, B) => C): Either[Exception, C] = {
+      (ea, eb) match {
+        case (Right(a), Right(b)) => {
+          try {
+            Right(f(a, b))
+          }
+          catch {
+            case e: Exception => {println(e); Left(e)}
+          }
+        }
+        case _ => {println("pelle"); Left(new Exception("Pelle was here, at least one computation failed."))}
+      }
+    }
+
     def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = {
       fork(sequence(ps.map(asyncF(f))))
     }
+  }
+
+  def testLazyException() = {
+
+    def f(ex: Boolean): String = {
+      if (ex) {
+        throw new NullPointerException("pelle")
+      }
+      "ole" + "dole"
+    }
+
+    def executor(s: => String): Unit = {
+      println("ex1")
+      println(s)
+      println("ex2")
+    }
+
+    println("before execute")
+    executor(f(true))
+
+    // Exception is thrown when s is evaluated (at println(s) )
+
   }
 
   object TestingActors {
@@ -125,9 +162,13 @@ object Chapter7NonBlocking {
       echoer ! "hei"
       echoer ! "hei2"
 
-//      val p = parMap(List.range(1, 1000))(math.sqrt(_))
-      val p = parMap(List.range(1, 1000))(i => if (i == 100) throw new NullPointerException("pelle") else math.sqrt(i))
+//      val p = parMap(List.range(1, 100))(math.sqrt(_))
+     val p = parMap(List.range(1, 100))(i => if (i == 10) throw new NullPointerException("pelle") else math.sqrt(i))
       val x = run(es)(p)
+      x match {
+        case Left(exception) => exception.printStackTrace()
+        case Right(a) => println(a)
+      }
       println(x)
 
       es.shutdown()
@@ -135,6 +176,7 @@ object Chapter7NonBlocking {
   }
 
   def main(args: Array[String]): Unit = {
+//    testLazyException()
     TestingActors.test()
   }
 
