df60da645abb394aaeaf7b4a9d45ad2ebef545f2
oyvind <oyvind@jergan.no>
2020-11-30 15:11:52 +0100

    ch9

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter9.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter9.scala
@@ -4,7 +4,10 @@ object Chapter9 {
 
   trait Parsers[ParseError, Parser[+_]] {
 
+    def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
     def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+    def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
 
     implicit def string(s: String): Parser[String]
     implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
@@ -15,6 +18,32 @@ object Chapter9 {
       def or[B>:A](p2: => Parser[B]): Parser[B] = Parsers.this.or(p, p2)
     }
 
+    object Hei {
+
+      val v = "pelle"
+      val v2: Parser[String] = "hei"
+
+      val p: Parser[Int] = ???
+      val p2: ParserOps[Int] = p
+      p2.|(p)
+
+
+      p | p
+
+      val v3 = asStringParser(42)(i => "hei")
+    }
+
+  }
+
+  object Ex1 {
+
+//    import Chapter9.Parsers
+
+//    val p: Parsers[String, List[_]] = ???
+
+//    import p._
+
+//    val v = "ole" | "dole"
   }
 
 
