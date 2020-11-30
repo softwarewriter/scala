9caede1ef63c3ceee77aef0aa4bc2cc94268d5e8
oyvind <oyvind@jergan.no>
2020-11-30 15:44:43 +0100

    ch9

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter9.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter9.scala
@@ -1,13 +1,23 @@
 package no.jergan.scrapbook.fpinscala
 
+import no.jergan.scrapbook.fpinscala.Chapter8.Prop.forAll
+import no.jergan.scrapbook.fpinscala.Chapter8.{Gen, Prop}
+
 object Chapter9 {
 
   trait Parsers[ParseError, Parser[+_]] {
 
     def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
+    def char(c: Char): Parser[Char]
+
     def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
     def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+    def many[A](p: Parser[A]): Parser[List[A]]
+
+    def map[A, B](a: Parser[A])(f: A => B): Parser[B]
 
     implicit def string(s: String): Parser[String]
     implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
@@ -16,6 +26,16 @@ object Chapter9 {
     case class ParserOps[A](p: Parser[A]) {
       def |[B>:A](p2: Parser[B]): Parser[B] = Parsers.this.or(p, p2)
       def or[B>:A](p2: => Parser[B]): Parser[B] = Parsers.this.or(p, p2)
+      def many: Parser[List[A]] = Parsers.this.many(p)
+      def map[B](f: A => B): Parser[B] = Parsers.this.map(p)(f)
+
+      object Laws {
+        def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+          forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+        def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop = equal(p, p.map(a => a))(in)
+      }
+
     }
 
     object Hei {
