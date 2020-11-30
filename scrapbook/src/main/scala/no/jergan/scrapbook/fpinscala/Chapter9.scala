d1483c2efc94d65995fd2855757bc39cf7f8b98f
oyvind <oyvind@jergan.no>
2020-11-30 16:33:59 +0100

    ch9

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter9.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter9.scala
@@ -9,7 +9,14 @@ object Chapter9 {
 
     def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
-    def char(c: Char): Parser[Char]
+    def char(c: Char): Parser[Char] = {
+      string(c.toString).map(_.charAt(0))
+    }
+
+    def succeed[A](a: A): Parser[A] = {
+      val v: Parser[String] = string("")
+      string("").map(_ => a)
+    }
 
     def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
 
@@ -17,17 +24,32 @@ object Chapter9 {
 
     def many[A](p: Parser[A]): Parser[List[A]]
 
+    def many1[A](p: Parser[A]): Parser[List[A]] = {
+      map2(p, many(p))((a, b) => a :: b)
+    }
+
     def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+
+    def map2[A, B, C](a: Parser[A], b: Parser[B])(f: (A, B) => C): Parser[C]
+
+    def flatMap[A, B](a: Parser[A])(f: A => Parser[B]): Parser[B]
+
+    def slice[A](p: Parser[A]): Parser[String]
 
     implicit def string(s: String): Parser[String]
     implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
     implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+//    implicit def a[A](a: A): Parser[A]
+//    implicit def asAParser[A](a: A)(implicit f: A => Parser[A]): ParserOps[A] = ParserOps(f(a))
 
     case class ParserOps[A](p: Parser[A]) {
       def |[B>:A](p2: Parser[B]): Parser[B] = Parsers.this.or(p, p2)
       def or[B>:A](p2: => Parser[B]): Parser[B] = Parsers.this.or(p, p2)
       def many: Parser[List[A]] = Parsers.this.many(p)
       def map[B](f: A => B): Parser[B] = Parsers.this.map(p)(f)
+      def flatMap[B](f: A => Parser[B]): Parser[B] = Parsers.this.flatMap(p)(f)
+      def slice: Parser[String] = Parsers.this.slice(p)
 
       object Laws {
         def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
