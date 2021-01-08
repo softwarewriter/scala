f11f4f282b98ab7e499121be2643b0b7f322fdf9
oyvind <oyvind@jergan.no>
2021-01-08 15:51:36 +0100

    Ch10

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter10.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter10.scala
@@ -54,6 +54,7 @@ object Chapter10 {
       }
       override val zero: A => A = identity
     }
+
   }
 
   object Ex4 {
@@ -123,6 +124,11 @@ object Chapter10 {
 
   object Ex6 {
 
+    def inverseEndoMonoid[A](m: Monoid[A => A]): Monoid[A => A] = new Monoid[A => A] {
+      override def op(a1: A => A, a2: A => A): A => A = m.op(a2, a1)
+      override val zero: A => A = m.zero
+    }
+
     def curryTest[A, B, C]() {
       def f(a: A, b: B): C = ???
       def ab(a: A): B => C = (b: B) => f(a, b)
@@ -130,22 +136,13 @@ object Chapter10 {
       def abc: A => B => C = (a: A) => (b: B) => f(a, b)
     }
 
-    /*
-    def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
-
-      def atoB: A => B = a => f(z, a)
-      def btoB(a: A): B => B = (b: B) => f(b, a)
-
-      val m: Monoid[B] = new Monoid[B] {
-        override def op(b1: B, b2: B): B = ???
-        override val zero: B = z
-      }
-
-      val b = Ex5.foldMap[A, B](as, m)(atoB)
-      b
+    def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = {
+      val aToBtoB: A => B => B = (a: A) => (b: B) => f(b, a)
+      val m: Monoid[B => B] = inverseEndoMonoid(Ex3.endoMonoid[B])
+      val bToB: B => B = Ex5.foldMap[A, B => B](as, m)(aToBtoB)
+      bToB(z)
     }
 
-     */
     def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = {
       val aToBtoB: A => B => B = (a: A) => (b: B) => f(a, b)
       val m: Monoid[B => B] = Ex3.endoMonoid[B]
@@ -154,8 +151,8 @@ object Chapter10 {
     }
 
     def test(): Unit = {
-//      println(foldLeft[String, String](List("ole", "dole", "doff"), "")(_ + _))
-      println(foldRight[String, String](List("ole", "dole", "doff"))("hei")(_ + _))
+      println(foldLeft[String, String](List("ole", "dole", "doff"))("left")(_ + _))
+      println(foldRight[String, String](List("ole", "dole", "doff"))("right")(_ + _))
     }
 
   }
