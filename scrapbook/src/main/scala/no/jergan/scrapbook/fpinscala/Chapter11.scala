700b2a4f82e6511daf53ffd8313a9e306f570553
oyvind <oyvind@jergan.no>
2021-01-17 14:34:25 +0100

    Ch11

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter11.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter11.scala
@@ -71,8 +71,16 @@ object Chapter11 {
         }
       }
 
+      def sequenceUsingFoldRight[A](lma: List[F[A]]): F[List[A]] = {
+        lma.foldRight(unit(List[A]()))((a, b) => map2(a, b)(_ :: _))
+      }
+
       def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] = {
         sequence(la.map(f))
+      }
+
+      def traverseUsingFoldRight[A, B](la: List[A])(f: A => F[B]): F[List[B]] = {
+        la.foldRight(unit(List[B]()))((a, b) => map2(f(a), b)(_ :: _))
       }
 
       def replicateM[A](n: Int, ma: F[A]): F[List[A]] = {
