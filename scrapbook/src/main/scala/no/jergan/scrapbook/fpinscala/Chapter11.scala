b848de95a2fd72b64eefd2081aac3d5710a1c5ef
oyvind <oyvind@jergan.no>
2021-01-17 12:30:44 +0100

    Ch11

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter11.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter11.scala
@@ -1,6 +1,18 @@
 package no.jergan.scrapbook.fpinscala
 
+import no.jergan.scrapbook.fpinscala.Chapter7.Par
+import no.jergan.scrapbook.fpinscala.Chapter5.Stream
+import no.jergan.scrapbook.fpinscala.Chapter9.Parsers
+
 object Chapter11 {
+
+  trait JFunctor[A] {
+    def map[B](f: A => B):  JFunctor[B]
+
+    def andThen[B, C](f: A => B)(g: B => C): JFunctor[C] = {
+      map(f).map(g)
+    }
+  }
 
   trait Functor[F[_]] {
     def map[A, B](fa: F[A])(f: A => B): F[B]
@@ -36,7 +48,60 @@ object Chapter11 {
        */
     }
 
-}
+    trait Monad[F[_]] extends Functor[F] {
+
+      def unit[A](a: A): F[A]
+
+      def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+      //        map2[A, A, B](fa, fa)((a1, a2) => f(a1))
+
+      def map[A, B](fa: F[A])(f: A => B): F[B] = {
+        flatMap(fa)(a => unit(f(a)))
+      }
+
+      def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = {
+        flatMap(fa)(a => map(fb)(b => f(a, b)))
+      }
+
+    }
+
+    object Monad {
+
+      val parMonad = new Monad[Par] {
+        override def unit[A](a: A): Par[A] = Par.unit(a)
+        override def flatMap[A, B](fa: Par[A])(f: A => Par[B]): Par[B] = Par.flatMap(fa)(f)
+      }
+
+      def parserMonad[P[+_]](p: Parsers[P]) = new Monad[P] {
+        override def unit[A](a: A): P[A] = p.succeed(a)
+        override def flatMap[A, B](fa: P[A])(f: A => P[B]): P[B] = p.flatMap(fa)(f)
+      }
+
+      val optionMonad = new Monad[Option] {
+        override def unit[A](a: A): Option[A] = Some(a)
+        override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+      }
+
+      /*
+         Should compile when we do chapter 5 according to new version.
+      val streamMonad = new Monad[Stream] {
+        override def unit[A](a: A): Stream[A] = Stream.apply(a)
+        override def flatMap[A, B](fa: Stream[A])(f: A => Stream[B]): Stream[B] = fa.flatMap(f)
+      }
+
+       */
+
+      val listMonad = new Monad[List] {
+        override def unit[A](a: A): List[A] = List(a)
+        override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa.flatMap(f)
+      }
+
+    }
+
+  }
+
+  object Ex1 {
+  }
 
   def main(args: Array[String]): Unit = {
 
