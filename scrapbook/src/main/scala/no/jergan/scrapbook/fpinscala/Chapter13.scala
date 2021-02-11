396d9619e53d9e4eb36863a4ffe4ea7181d4053f
oyvind <oyvind@jergan.no>
2021-02-11 16:24:19 +0100

    ch13

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter13.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter13.scala
@@ -5,24 +5,10 @@ import no.jergan.scrapbook.fpinscala.Chapter11.Monad
 
 object Chapter13 {
 
-
   sealed trait Free[F[_], A] {
 
-    /*
-    def map[B](fa: Free[F, A])(f: A => B): Free[F, B] = {
-     fa match {
-        case Return(a) => Return(f(a))
-        case Suspend(s) => FlatMap[F, A, B](Suspend(s), a => Return(f(a)))
-        case FlatMap(s, ff) => {
-          val ss: Free[F, A] = s
-          val fff: Nothing => Free[F, A] = ff
-        }
-          FlatMap(s, a => f(a))
-        }
-    }
-
-     */
-
+    def map[B](f: A => B): Free[F, B] = flatMap(a => Return(f(a)))
+    def flatMap[B](f: A => Free[F, B]): Free[F, B] = FlatMap(this, f)
   }
 
   case class Return[F[_], A](a: A) extends Free[F, A]
@@ -34,19 +20,46 @@ object Chapter13 {
     override def flatMap[A, B](fa: Free[F, A])(f: A => Free[F, B]): Free[F, B] = FlatMap(fa, f)
   }
 
+  @annotation.tailrec
+  def runTrampoline[A](freeA: Free[Function0, A]): A = {
+    freeA match {
+      case Return(a) => a
+      case Suspend(s) => s.apply()
+      case FlatMap(s, f) => s match {
+        case Return(a) => runTrampoline(f(a))
+        case Suspend(s) => runTrampoline(f(s.apply()))
+        case FlatMap(ss, g) => runTrampoline(ss.flatMap(a => g(a)).flatMap(f))
+      }
+    }
+  }
+
+  object Ex1 {
+    // implemented map and flatMap in trait Free
+    // implemented def freeMonad
+  }
+
+  object Ex2 {
+    // implemented runTrampoline. IntelliJ struggles with the types but solution is correct and compiles with sbt.
+  }
+
   def main(args: Array[String]): Unit = {
 
-
-    def f: Int => Int = a => {
-//      println("f")
-      a
-    }
-
-    val g = List.fill(10000)(f).foldLeft(f)(_ compose _)
-
-    println(g(42))
-
   }
+
+
+  /*
+  @annotation.tailrec
+  def runTrampoline[A](a: Free[Function0,A]): A = (a) match {
+    case Return(a) => a
+    case Suspend(r) => r()
+    case FlatMap(x,f) => x match {
+      case Return(a) => runTrampoline { f(a) }
+      case Suspend(r) => runTrampoline { f(r()) }
+      case FlatMap(a0,g) => runTrampoline { a0 flatMap { a0 => g(a0) flatMap f } }
+    }
+  }
+
+   */
 
 
 }
