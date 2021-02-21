1dd467a99f0b9dcbdc12be5e7d4f26001dd69539
oyvind <oyvind@jergan.no>
2021-02-21 14:20:09 +0100

    ch14

--- scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter14.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/fpinscala/Chapter14.scala
@@ -208,29 +208,11 @@ object Chapter14 {
   }
 
   object Ex3 {
-    // Created trait for mutable map
+    // Created trait for mutable map (STMap)
   }
 
   def main(args: Array[String]): Unit = {
-    /*
-  val p: RunnableST[(Int, Int)] = new RunnableST[(Int, Int)] {
-    def apply[S] = for {
-      r1 <- STRef(1)
-      r2 <- STRef(2)
-      x  <- r1.read
-      y  <- r2.read
-      _  <- r1.write(y + 1)
-      _  <- r2.write(x + 1)
-      a  <- r1.read
-      b <- r2.read
-    } yield (a, b)
-  }
-  val r1: (Int, Int) = ST.runST(p)
-  println(r1)
-
-     */
-//    println(quicksort(List(6, 4, 1, 5, 7, 2, 3, 8)))
-
+    println(quicksort(List(6, 4, 1, 5, 7, 2, 3, 8)))
     println(mapProgram(Map("ole" -> 42)))
   }
 
