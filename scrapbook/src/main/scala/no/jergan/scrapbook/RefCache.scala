71dcc60da44f89caa525c1009d3be56deaf75af9
oyvind <oyvind@jergan.no>
2021-09-27 09:40:59 +0200

    Cache.

--- scrapbook/src/main/scala/no/jergan/scrapbook/RefCache.scala
+++ scrapbook/src/main/scala/no/jergan/scrapbook/RefCache.scala
@@ -5,7 +5,7 @@ import cats.effect.concurrent.Ref
 import cats.effect.{ExitCode, IO, IOApp, Resource, Sync, Timer}
 import cats.implicits._
 import fs2.{RaiseThrowable, Stream}
-import no.jergan.scrapbook.RefCache.MemoryStore.Entry
+import no.jergan.scrapbook.RefCache.MemoryStore.{Entry, State}
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.{DurationInt, FiniteDuration, SECONDS}
@@ -21,41 +21,56 @@ object RefCache {
 
     def get(key: K): F[Option[V]]
     def put(key: K, value: V): F[Unit]
+    def clear(): F[Unit]
 
   }
 
-  class MemoryStore[F[_]: Sync: Timer, K, V](ref: Ref[F, Map[K, Entry[V]]], timeout: Option[Long]) extends Store[F, K, V] {
+  class MemoryStore[F[_]: Sync: Timer, K, V](ref: Ref[F, State[K, V]], timeout: Option[Long]) extends Store[F, K, V] {
 
     override def get(key: K): F[Option[V]] =
       for {
-        now     <- Timer[F].clock.monotonic(TimeUnit.MILLISECONDS)
-        current <- ref.get
-        entry   = current.get(key)
-        value   <- entry match {
-          case None                              => Applicative[F].pure(None)
-          case Some(e) if timeout.fold(false)(e.time < now - _) => ref.set(current.removed(key)).map(_ => None)
-          case Some(e)                           => Applicative[F].pure(Some(e.value))
-        }
-      } yield {
-        value
-      }
+        now <- Timer[F].clock.monotonic(TimeUnit.MILLISECONDS)
+        value <- ref.modify(state => state.get(key, now, timeout))
+      } yield value
 
     override def put(key: K, value: V): F[Unit] =
       for {
         now     <- Timer[F].clock.monotonic(TimeUnit.MILLISECONDS)
-        current <- ref.get
-        _       <- ref.set(current + (key -> Entry(value, now)))
+        _       <- ref.update(state => state.put(key, value, now))
+      } yield {}
+
+    override def clear(): F[Unit] =
+      for {
+        _       <- ref.set(State())
       } yield {}
 
   }
 
   object MemoryStore {
 
+    case class State[K, V](map: Map[K, Entry[V]]) {
+
+      def get(key: K, now: Long, timeout: Option[Long]): (State[K, V], Option[V]) =
+        map.get(key) match {
+          case None                                             => (this, None)
+          case Some(e) if timeout.fold(false)(e.time < now - _) => (State(map.removed(key)), None)
+          case Some(e)                                          => (this, Some(e.value))
+        }
+
+      def put(key: K, value: V, now: Long): State[K, V] =
+        State(map + (key -> Entry(value, now)))
+    }
+
+    object State {
+
+      def apply[K, V](): State[K, V] = State(Map.empty)
+    }
+
     case class Entry[V](value: V, time: Long)
 
-    def apply[F[_]: Sync: Timer, K, V](timeout: Option[Long]): F[Store[F, K, V]] =
+    def apply[F[_]: Sync: Timer, K, V](maxSize: Option[Long] = None, timeout: Option[Long] = None): F[Store[F, K, V]] =
       Ref
-        .of[F, Map[K, Entry[V]]](Map())
+        .of[F, State[K, V]](State())
         .map(new MemoryStore[F, K, V](_, timeout))
 
   }
--- scrapbook/src/test/scala/no/jergan/scrapbook/MemoryStoreSpec.scala
+++ scrapbook/src/test/scala/no/jergan/scrapbook/MemoryStoreSpec.scala
@@ -1,15 +1,15 @@
 package no.jergan.scrapbook
 
-import cats.effect.{ExitCode, IO, Timer}
+import cats.effect.{IO, Timer}
 import no.jergan.scrapbook.RefCache.MemoryStore
 
 import scala.concurrent.duration.{FiniteDuration, SECONDS}
 
 class MemoryStoreSpec extends IOFunSuite {
 
-  test("MemoryStore with timeout") = {
+  test("Test basic") = {
     for {
-      store <- MemoryStore[IO, String, Int](Some(100))
+      store <- MemoryStore[IO, String, Int]()
       val1  <- store.get("k1")
       _     <- store.put("k1", 42)
       val2  <- store.get("k1")
@@ -22,9 +22,30 @@ class MemoryStoreSpec extends IOFunSuite {
     }
   }
 
-  test("MemoryStore without timeout") = {
+  /*
+  test("Test LRU") = {
     for {
-      store <- MemoryStore[IO, String, Int](None)
+      store <- MemoryStore[IO, String, Int](maxSize = Some(2))
+      _     <- store.put("k1", 42)
+      _     <- store.put("k2", 43)
+      val21  <- store.get("k2")
+      val11  <- store.get("k1")
+      _     <- store.put("k3", 44)
+      val12  <- store.get("k1")
+      val22  <- store.get("k2")
+    } yield {
+      assert(val11.contains(42))
+      assert(val21.contains(43))
+      assert(val12.contains(42))
+      assert(val22.isEmpty)
+    }
+  }
+
+   */
+
+  test("Test timeout") = {
+    for {
+      store <- MemoryStore[IO, String, Int](timeout = Some(100))
       val1  <- store.get("k1")
       _     <- store.put("k1", 42)
       _     <- store.put("k2", 43)
@@ -37,6 +58,21 @@ class MemoryStoreSpec extends IOFunSuite {
       assert(val2.contains(42))
       assert(val3.contains(43))
       assert(val4.contains(44))
+    }
+  }
+
+  test("Test clear") = {
+    for {
+      store <- MemoryStore[IO, String, Int]()
+      val1  <- store.get("k1")
+      _     <- store.put("k1", 42)
+      val2  <- store.get("k1")
+      _     <- store.clear()
+      val3  <- store.get("k1")
+    } yield {
+      assert(val1.isEmpty)
+      assert(val2.contains(42))
+      assert(val3.isEmpty)
     }
   }
 
